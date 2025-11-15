3216f54e272ed8fdbcb9c7663ba9384f17321123
varghese <varghesevj33@gmail.com>
2025-11-16 03:04:15 +0530

    cloudinary logic fix check 1

--- dubee/settings.py
+++ dubee/settings.py
@@ -230,13 +230,13 @@ if os.getenv("CLOUDINARY_URL"):
 
     DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
-    CLOUDINARY_STORAGE = {
-        "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
-        "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
-        "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
-    }
+    # CLOUDINARY_STORAGE = {
+    #     "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
+    #     "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
+    #     "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
+    # }
 
-    MEDIA_URL = '/'
+    # MEDIA_URL = '/'
 else:
     # Local development (store files on disk)
     print("Using local storage for media files")
